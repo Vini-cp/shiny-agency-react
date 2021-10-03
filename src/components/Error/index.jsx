b1b62709044033ba2a3c11d3b72f7a98efd0e14b
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-10-03 20:13:20 -0300

    Add Error route

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <div>
+      <h1>Oups 🙈 This page does not exist!</h1>
+    </div>
+  );
+}
+
+export default Error;
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/index';
 import Survey from './pages/Survey/index';
-import Header from './components/Header';
+import Header from './components/Header/index';
+import Error from './components/Error/index';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey/:questionNumber">
-        <Survey />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
