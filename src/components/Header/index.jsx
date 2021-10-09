f8f1498f6d8b0d589548bc327840b2a9971fd0df
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-10-09 18:16:16 -0300

    Edit Home Page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,45 +3,49 @@ import styled from 'styled-components';
 import colors from '../../utils/style/colors';
 import Logo from '../../assets/shiny-logo.png';
 
-const HeaderLogo = styled.img`
-    height: 70px;
-    padding: 30px 0 30px 33px;
-    border-radius: 50%;
+const StyledHeader = styled.div`
+  position: absolute;
+  height: 200px;
+  width: 1440px;
+  display: flex;
+  flex-direction: row;
 `;
 
-const StyledHeader = styled.div`
-    height: 130px;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
+const HeaderLogo = styled.img`
+  position: absolute;
+  width: 187.63px;
+  height: 70px;
+  left: 33px;
+  top: 30px;
 `;
 
 const ImageDiv = styled.div`
-    height: 100px;
-    width: 30%;
+  height: 100px;
+  width: 30%;
 `;
 
 const NavigationDiv = styled.div`
-    height: 100px;
-    width: 80%;
-    display: flex;
-    justify-content: right;
-    align-items: center;
+  height: 100px;
+  width: 80%;
+  padding-right: 81px;
+  display: flex;
+  justify-content: right;
+  align-items: center;
 `;
 
 const NavText = styled.nav`
-    vertical-align: middle;
-    display: inline-block;
+  vertical-align: middle;
+  display: inline-block;
 `;
 
 const StyledLink = styled(Link)`
-    padding: 15px;
-    color: #8186a0;
-    text-decoration: none;
-    font-size: 20px;
-    ${( {isFullLink} ) =>
-        isFullLink && 
-        `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 20px;
+  ${( {isFullLink} ) =>
+      isFullLink && 
+      `color: white; border-radius: 30px; background-color: ${colors.primary};`}
 `;
 
 function Header() {
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,84 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import HomePicture from '../../assets/home-page-img.svg';
+
+const HomeDiv = styled.div`
+  position: absolute;
+  width: 1313px;
+  height: 824px;
+  left: 65px;
+  top: 200px;
+  background-color: ${colors.backgroundLight}
+`;
+
+const TextDiv = styled.div`
+  position: absolute;
+  width: 552px;
+  height: 249px;
+  left: 98px;
+  top: 176px;
+`;
+
+const Text = styled.p`
+  font-family: Trebuchet MS;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 160.5%;
+  color: #2F2E41;
+  margin: 0;
+`;
+
+const ImageDiv = styled.div`
+  position: absolute;
+  width: 541px;
+  height: 506px;
+  left: 695px;
+  top: 139px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const ButtonDiv = styled.div`
+  position: absolute;
+  width: 261px;
+  height: 40px;
+  left: 91px;
+  top: 556px;
+  border-radius: 29px;
+  background-color: ${colors.primary}
+`;
+
+const NavText = styled.nav`
+  text-align: center;
+  padding-top: 7px;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 20px;
+  text-decoration: none;
+  color: white;
+`;
+
 function Home() {
   return (
-    <div>
-      <h1> Home Page 🏠</h1>
-    </div>
+    <HomeDiv>
+      <TextDiv>
+        <Text>Identify your needs, we take care of the rest, with the best talents</Text>
+      </TextDiv>
+      <ImageDiv>
+        <Image src={HomePicture}/>
+      </ImageDiv>
+      <ButtonDiv>
+        <NavText>
+          <StyledLink to="/survey/1"> Take the survey </StyledLink>
+        </NavText>
+      </ButtonDiv>
+    </HomeDiv>
   );
 }
 
