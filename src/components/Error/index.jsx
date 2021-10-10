cd2d0c9757d19bcb82836d7b44c56c011c1084cb
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-10-09 22:38:49 -0300

    Edit Error Page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,59 @@
+import styled from 'styled-components';
+import Image from '../../assets/page-404.svg'
+import colors from '../../utils/style/colors';
+
+const ErrorDiv = styled.div`
+  position: absolute;
+  width: 1313px;
+  height: 1256px;
+  left: 63px;
+  top: 189px;
+
+  background: ${colors.backgroundLight};
+`;
+
+const OupsDiv = styled.div`
+  position: absolute;
+  width: 463px;
+  height: 77px;
+  left: 425px;
+  top: 99px;
+`;
+
+const ImageDiv = styled.img`
+  position: absolute;
+  width: 875px;
+  height: 476px;
+  left: 201px;
+  top: 219px;
+`;
+
+const MessageDiv = styled.div`
+  position: absolute;
+  width: 878px;
+  height: 127px;
+  left: 208px;
+  top: 765px;
+`;
+
+const Text = styled.div`
+  font-family: Trebuchet MS;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 31px;
+  line-height: 36px;
+  text-align: center;
+
+  color: ${colors.textcolor};
+`;
+
 function Error() {
   return (
-    <div>
-      <h1>Oups 🙈 This page does not exist!</h1>
-    </div>
+    <ErrorDiv>
+      <OupsDiv><Text>Oops...</Text></OupsDiv>
+      <ImageDiv src={Image} />
+      <MessageDiv><Text>It looks like there is a problem.</Text></MessageDiv>
+    </ErrorDiv>
   );
 }
 
